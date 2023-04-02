5497119e9d0e6c5dc5aca5859167b9ac4264e2f8
nhatduy2210 <115464044+nhatduy2210@users.noreply.github.com>
2023-04-02 21:05:51 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng Sinh Nhật bé vợ anh';
+const titleCard = 'For you';
+const contentCard = 'Chúc em có một ngày sinh nhật thật vui.Hãy luôn xinh đẹp và cười tươi trên con đường mà em đã chọn nhé. Chúc mọi thứ tốt đẹp dành cho em cả ngày hôm nay và mãi về sau💘';
+
+
+const giftImage = 'z4232360324637_2f0f462bf97d61c2660d632f4270d514.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
